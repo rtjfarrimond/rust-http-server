1e42c6d1c8031ccdac8ba2fdbf2eceacda138c00
Robert Farrimond <robert.farrimond@ovoenergy.com>
2022-04-13 09:25:33 +0100

    Parse query param string; lifetime for buffer

--- src/http/request.rs
+++ src/http/request.rs
@@ -10,13 +10,13 @@ use std::fmt::Formatter;
 use std::str;
 use std::str::Utf8Error;
 
-pub struct Request {
-    path: String,
-    query_string: Option<String>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl Display for Request {
+impl<'buf> Display for Request<'buf> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), FmtError> {
         let query_string = match &self.query_string {
             Some(qs) => qs.to_string(),
@@ -30,24 +30,29 @@ impl Display for Request {
     }
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
-    fn try_from(buffer: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from(buffer: &'buf [u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buffer)?;
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path_and_query, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
 
-        let query_string: Option<String> = None;
+        let pivot_option = path_and_query.find('?');
+        let path = match pivot_option {
+            None => path_and_query,
+            Some(i) => &path_and_query[..i],
+        };
+        let query_string: Option<&str> = pivot_option.map(|i| &path_and_query[i + 1..]);
         let method: Method = method.parse()?;
 
         let request = Request {
-            path: path.to_string(),
+            path,
             query_string,
             method,
         };
